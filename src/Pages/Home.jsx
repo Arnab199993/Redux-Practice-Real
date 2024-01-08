cb48da14e13d2bf97837ecb0ab30cd24796ea418
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2024-01-08 16:43:29 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,3 @@
-// import React, { useState } from "react";
-// import Products from "../Components/Products";
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <h2>Welcome to the redux toolkit store</h2>
-//       <section>
-//         <h3>Products</h3>
-//         <Products />
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Home;
 import React, { useState } from "react";
 import Products from "../Components/Products";
 
